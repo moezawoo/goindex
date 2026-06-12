a084580efddfaca4628f2a9343ad4c919d106d05
Moe Zaw Oo <mzawoo@jhpiego.org>
2026-06-12 23:50:11 +0630

    Populate blank DownloadLandingPage component

---
+++ src/components/DownloadLandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DownloadLandingPage = () => {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h2>Movie Download Page</h2>
+      <p>Select a file from your Drive index to view details.</p>
+    </div>
+  );
+};
+
+export default DownloadLandingPage;
